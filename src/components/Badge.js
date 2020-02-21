8525555b41ec3e75f37123394d2fda3362d18df0
Carlos <carlos.e.santaniello@gmail.com>
2020-02-21 17:24:20 +0100

    Apply styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg'; 
 
 class Badge extends React.Component {
   render() {
     return (
-    <div>
-      <div>
+    <div className="Badge">
+      <div className="Badge__header">
         <img src={confLogo} alt="Logo de la conferencia"></img>
       </div>
     
-      <div>
-        <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+      <div className="Badge__section-name">
+        <img 
+        className="Badge__avatar"
+        src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
         <h1>Carlos <br/> Santaniello</h1>
       </div>
 
-      <div>
-        <p>Desarrollador Frontend</p>
-        <p>@cesantaniello</p>
+      <div className="Badge__section-info">
+        <h3>Desarrollador Frontend</h3>
+        <div>@cesantaniello</div>
       </div>
 
-      <div>
+      <div className="Badge__footer">
         #PlatziConf
       </div>
     </div>
