51c8b2f37265459ede17bfc6d82ff1b85c5391dc
Carlos <carlos.e.santaniello@gmail.com>
2020-02-24 16:59:48 +0100

    Start up stat

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,93 +1,91 @@
-import React from 'react'
+import React from 'react';
 
 class BadgeForm extends React.Component {
-  state = {
-    jobTitle: 'Designer',
-  };
-  handleChange = e => {
-  /*  console.log({
-      name: e.target.name,
-      value: e.target.value,
-    });*/
+  // state = {};
 
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
+  // handleChange = e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
 
   handleClick = e => {
     console.log('Button was clicked');
   };
-  
+
   handleSubmit = e => {
     e.preventDefault();
     console.log('Form was submitted');
     console.log(this.state);
-  };  
+  };
 
-  render () {
+  render() {
     return (
       <div>
         <h1>New Attendant</h1>
 
         <form onSubmit={this.handleSubmit}>
-          <div classname="form-group">
+          <div className="form-group">
             <label>First Name</label>
-            <input onChange={this.handleChange} 
-            className="form-control" 
-            type="text" 
-            name="firstName" 
-            value={this.state.firstName}
-            />            
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+              value={this.props.formValues.firstName}
+            />
           </div>
 
-          <div classname="form-group">
+          <div className="form-group">
             <label>Last Name</label>
-            <input onChange={this.handleChange} 
-            className="form-control" 
-            type="text" 
-            name="lastName" 
-            value={this.state.lastName}
-            />            
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.props.formValues.lastName}
+            />
           </div>
-          
-          <div classname="form-group">
+
+          <div className="form-group">
             <label>Email</label>
-            <input onChange={this.handleChange} 
-            className="form-control" 
-            type="email" 
-            name="email" 
-            value={this.state.email}
-            />            
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.props.formValues.email}
+            />
           </div>
 
-          <div classname="form-group">
+          <div className="form-group">
             <label>Job Title</label>
-            <input onChange={this.handleChange} 
-            className="form-control" 
-            type="text" 
-            name="jobTitle" 
-            value={this.state.jobTitle}
-            />            
-          </div>      
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.props.formValues.jobTitle}
+            />
+          </div>
 
-          <div classname="form-group">
+          <div className="form-group">
             <label>Twitter</label>
-            <input onChange={this.handleChange} 
-            className="form-control" 
-            type="text" 
-            name="twitter" 
-            value={this.state.twitter}
-            />            
-          </div>                                  
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.props.formValues.twitter}
+            />
+          </div>
 
-          <button  
-          onClick={this.handleClick} 
-          className="btn btn-primary">Save</button>
+          <button onClick={this.handleClick} className="btn btn-primary">
+            Save
+          </button>
         </form>
       </div>
-
-    )
+    );
   }
 }
 
