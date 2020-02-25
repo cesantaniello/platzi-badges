28f8c9483a52cfe62e9f4583723bbcc5fa8dc7b7
Carlos <carlos.e.santaniello@gmail.com>
2020-02-25 22:55:23 +0100

    React Fragment

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,7 +4,7 @@ import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
-import Navbar from '../components/Navbar';
+
 
 class BadgeNew extends React.Component {
   state = {
@@ -28,8 +28,8 @@ class BadgeNew extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
+      <React.Fragment>
+        
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo" />
         </div>
@@ -55,7 +55,7 @@ class BadgeNew extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
