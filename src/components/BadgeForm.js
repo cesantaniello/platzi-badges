2cc6420f30fd8342f93d8644e8754de22849e7e7
Carlos <carlos.e.santaniello@gmail.com>
2020-03-02 18:37:59 +0100

    PUT Method

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -5,17 +5,9 @@ class BadgeForm extends React.Component {
     console.log('Button was clicked');
   };
 
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   console.log('Form was submitted');
-  //   console.log(this.state);
-  // };
-
   render() {
     return (
       <div>
-        <h1>New Attendant</h1>
-
         <form onSubmit={this.props.onSubmit}>
           <div className="form-group">
             <label>First Name</label>
@@ -76,7 +68,9 @@ class BadgeForm extends React.Component {
             Save
           </button>
 
-          {this.props.error && <p className="text-danger">{this.props.error.message}</p>}
+          {this.props.error && (
+            <p className="text-danger">{this.props.error.message}</p>
+          )}
         </form>
       </div>
     );
